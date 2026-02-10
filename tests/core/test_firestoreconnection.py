093824329170bbb809aba411197c3436d88c1307
Patrick Schröder <patrickschroder479@gmail.com>
2026-02-10 14:42:25 +0100

    Started work on FirestoreConnection tests
    Prepared first tests for FirestoreConnection class.

---
+++ tests/core/test_firestoreconnection.py
@@ -0,0 +1,35 @@
+import unittest
+from src.core.firestoreconnection import FirestoreConnection
+
+
+class TestFirestoreConnection(unittest.TestCase):
+    """Class with tests of the FirestoreConnection."""
+
+    def test_init(self):
+        """Method tests the initialization of a class"""
+        firestore_connection = FirestoreConnection("example_path")
+
+        path = "example_path"
+        credentials = None
+
+        self.assertEqual(firestore_connection.get_path(), path)
+        self.assertEqual(firestore_connection.credentials, credentials)
+
+    def test_set_path(self):
+        """Method tests the setting the path of firestore connection."""
+        firestore_connection = FirestoreConnection("example_path")
+        path = "example_path_test"
+
+        firestore_connection.set_path(path)
+        self.assertEqual(firestore_connection.get_path(), path)
+
+        del firestore_connection
+
+    def test_get_path(self):
+        """Method tests the getting the path of firestore connection."""
+        firestore_connection = FirestoreConnection("example_path")
+        path = "example_path"
+
+        self.assertEqual(firestore_connection.get_path(), path)
+
+        del firestore_connection
