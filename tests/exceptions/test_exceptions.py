10d9643d0200fea61052c4d63c3057e4b9e862f7
Patrick Schröder <patrickschroder479@gmail.com>
2026-03-28 14:23:43 +0100

    Refactored the code of Exceptions tests
    Used Black library to refactor the code of a class with custom exceptions tests.

--- tests/exceptions/test_exceptions.py
+++ tests/exceptions/test_exceptions.py
@@ -1,5 +1,10 @@
 import unittest
-from src.exceptions.exceptions import CredentialsError, DBError, CollectionIsNone, ConnectionNotConfigured
+from src.exceptions.exceptions import (
+    CredentialsError,
+    DBError,
+    CollectionIsNone,
+    ConnectionNotConfigured,
+)
 
 
 class TestExceptions(unittest.TestCase):
@@ -9,39 +14,45 @@ class TestExceptions(unittest.TestCase):
         self.messages_and_codes = {
             "CredentialsError": {
                 "message": "Provided credentials returned error.",
-                "code": "CREDENTIALS_INVALID_001"
+                "code": "CREDENTIALS_INVALID_001",
             },
             "DBError": {
                 "message": "DB connection error.",
-                "code": "DB_CONNECTION_ERROR_002"
+                "code": "DB_CONNECTION_ERROR_002",
             },
             "CollectionIsNone": {
                 "message": "Collection is None, check the credentials and connection.",
-                "code": "COLLECTION_NONE_ERROR_003"
+                "code": "COLLECTION_NONE_ERROR_003",
             },
             "ConnectionNotConfigured": {
                 "message": "Firestore connection not configured.",
-                "code": "CONNECTION_NOT_CONFIGURED_004"
-            }
+                "code": "CONNECTION_NOT_CONFIGURED_004",
+            },
         }
 
     def test_CredentialsError_init(self):
         """Method to test CredentialsError Exception initialization."""
         e = CredentialsError()
-        self.assertEqual(e.message, self.messages_and_codes["CredentialsError"]["message"])
+        self.assertEqual(
+            e.message, self.messages_and_codes["CredentialsError"]["message"]
+        )
         self.assertEqual(e.code, self.messages_and_codes["CredentialsError"]["code"])
         del e
 
     def test_CredentialsError_get_message(self):
         """Method to test CredentialsError Exception get_message method."""
         e = CredentialsError()
-        self.assertEqual(e.get_message(), self.messages_and_codes["CredentialsError"]["message"])
+        self.assertEqual(
+            e.get_message(), self.messages_and_codes["CredentialsError"]["message"]
+        )
         del e
 
     def test_CredentialsError_get_code(self):
         """Method to test CredentialsError Exception get_code method."""
         e = CredentialsError()
-        self.assertEqual(e.get_code(), self.messages_and_codes["CredentialsError"]["code"])
+        self.assertEqual(
+            e.get_code(), self.messages_and_codes["CredentialsError"]["code"]
+        )
         del e
 
     def test_DBError_init(self):
@@ -66,37 +77,52 @@ class TestExceptions(unittest.TestCase):
     def test_CollectionIsNone_init(self):
         """Method to test CollectionIsNone Exception initialization."""
         e = CollectionIsNone()
-        self.assertEqual(e.message, self.messages_and_codes["CollectionIsNone"]["message"])
+        self.assertEqual(
+            e.message, self.messages_and_codes["CollectionIsNone"]["message"]
+        )
         self.assertEqual(e.code, self.messages_and_codes["CollectionIsNone"]["code"])
         del e
 
     def test_CollectionIsNone_get_message(self):
         """Method to test CollectionIsNone Exception get_message method."""
         e = CollectionIsNone()
-        self.assertEqual(e.get_message(), self.messages_and_codes["CollectionIsNone"]["message"])
+        self.assertEqual(
+            e.get_message(), self.messages_and_codes["CollectionIsNone"]["message"]
+        )
         del e
 
     def test_CollectionIsNone_get_code(self):
         """Method to test CollectionIsNone Exception get_code method."""
         e = CollectionIsNone()
-        self.assertEqual(e.get_code(), self.messages_and_codes["CollectionIsNone"]["code"])
+        self.assertEqual(
+            e.get_code(), self.messages_and_codes["CollectionIsNone"]["code"]
+        )
         del e
 
     def test_ConnectionNotConfigured_init(self):
         """Method to test ConnectionNotConfigured Exception initialization."""
         e = ConnectionNotConfigured()
-        self.assertEqual(e.message, self.messages_and_codes["ConnectionNotConfigured"]["message"])
-        self.assertEqual(e.code, self.messages_and_codes["ConnectionNotConfigured"]["code"])
+        self.assertEqual(
+            e.message, self.messages_and_codes["ConnectionNotConfigured"]["message"]
+        )
+        self.assertEqual(
+            e.code, self.messages_and_codes["ConnectionNotConfigured"]["code"]
+        )
         del e
 
     def test_ConnectionNotConfigured_get_message(self):
         """Method to test ConnectionNotConfigured Exception get_message method."""
         e = ConnectionNotConfigured()
-        self.assertEqual(e.get_message(), self.messages_and_codes["ConnectionNotConfigured"]["message"])
+        self.assertEqual(
+            e.get_message(),
+            self.messages_and_codes["ConnectionNotConfigured"]["message"],
+        )
         del e
 
     def test_ConnectionNotConfigured_get_code(self):
         """Method to test ConnectionNotConfigured Exception get_code method."""
         e = ConnectionNotConfigured()
-        self.assertEqual(e.get_code(), self.messages_and_codes["ConnectionNotConfigured"]["code"])
+        self.assertEqual(
+            e.get_code(), self.messages_and_codes["ConnectionNotConfigured"]["code"]
+        )
         del e
