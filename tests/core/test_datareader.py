aaff8739ce87df57deb235187e834207e65e1514
Patrick Schröder <patrickschroder479@gmail.com>
2026-04-01 15:12:00 +0200

    Added tests for _normalize method of DataReader class
    Added new unit tests in TestDataReader class for _normalize method of DataReader class.

--- tests/core/test_datareader.py
+++ tests/core/test_datareader.py
@@ -10,3 +10,32 @@ class TestDataReader(unittest.TestCase):
         dr = DataReader()
         self.assertEqual(type(dr), DataReader)
         del dr
+
+    def test_normalize_none(self):
+        dr = DataReader()
+        with self.assertRaises(ValueError):
+            dr._normalize(None)
+
+    def test_normalize_not_iterable(self):
+        dr = DataReader()
+        with self.assertRaises(TypeError):
+            dr._normalize(123)
+
+    def test_normalize_string_iterable_rejected(self):
+        dr = DataReader()
+        with self.assertRaises(TypeError):
+            dr._normalize("string")
+
+    def test_normalize_empty_iterable(self):
+        dr = DataReader()
+        result = dr._normalize([])
+        self.assertEqual(result, [])
+
+    def test_normalize_dict_items(self):
+        dr = DataReader()
+        data = [{"a": 1}, {"b": 2}]
+        result = dr._normalize(data)
+
+        self.assertEqual(result, data)
+        self.assertIsNot(result[0], data[0])
+        
