fd90c1a2ce80573a7345989f491ae18a7c790f2a
Patrick Schröder <patrickschroder479@gmail.com>
2026-04-17 19:48:34 +0200

    Added test for success route of DataReaderUI.fetch_as_dicts
    Added test for success route of DataReaderUI.fetch_as_dicts in TestDataReaderUI class.

--- tests/interface/test_datareaderui.py
+++ tests/interface/test_datareaderui.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import MagicMock
 from src.interface.datareaderui import DataReaderUI
 
 
@@ -8,3 +9,13 @@ class TestDataReaderUI(unittest.TestCase):
     def test_init(self):
         ui = DataReaderUI()
         self.assertEqual(type(ui), DataReaderUI)
+
+    def test_fetch_as_dicts_success(self):
+        ui = DataReaderUI()
+        ui._DataReaderUI__dr = MagicMock()
+        ui._DataReaderUI__dr.fetch_as_dicts.return_value = [{"a": 1}]
+
+        result = ui.fetch_as_dicts("input")
+
+        self.assertEqual(result, [{"a": 1}])
+        ui._DataReaderUI__dr.fetch_as_dicts.assert_called_once_with("input")
